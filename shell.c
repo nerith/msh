56fcddb76e13b7dc5ff997d6bc4f983cf83df333
Brandon Fairchild <csbit32@gmail.com>
2016-02-13 11:02:50 -0500

    Add a method to show the prompt symbol

--- shell.c
+++ shell.c
@@ -6,6 +6,9 @@
 #include <stdio.h>
 
 void showShellMessage();
+void showPrompt();
+
+#define PROMPT_SYM '>'
 
 int main(int argc, char** argv)
 {
@@ -31,4 +34,17 @@ void showShellMessage()
     puts("Created by Brandon Fairchild\n");
     puts("Enter `help` at any time to get extra information.");
     puts("Type `exit` to close the shell.\n");
+}
+
+/**
+ * Displays the prompt symbol to let the user know they can enter
+ * a new command.
+ *
+ * By default, the prompt symbol is provided by PROMPT_SYM.
+ *
+ * @returns nothing
+ */
+void showPrompt()
+{
+    printf("%c ", PROMPT_SYM);
 }
