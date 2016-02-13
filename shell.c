d2654a496352ee7b703ef30920a0a4495639927b
Brandon Fairchild <csbit32@gmail.com>
2016-02-13 10:55:40 -0500

    Show an informational message on startup

--- shell.c
+++ shell.c
@@ -1,6 +1,34 @@
+/* Brandon Fairchild
+ * The MUSH Shell
+ * 2-11-2016
+ */
+
 #include <stdio.h>
+
+void showShellMessage();
 
 int main(int argc, char** argv)
 {
+    showShellMessage();
+
     return 0;
+}
+
+
+/**
+ * Displays information about the shell
+ *
+ * @returns nothing
+ */
+void showShellMessage()
+{
+    puts("\t   __  ___ __  __ ____ __ __");
+    puts("\t  /  |/  // / / // __// // /");
+    puts("\t / /|_/ // /_/ /_\\ \\ / _  /");
+    puts("\t/_/  /_/ \\____//___//_//_/\n");
+    puts("\tTHE MODULAR UNIVERSAL SHELL\n");
+    puts("Welcome to the MUSH shell!");
+    puts("Created by Brandon Fairchild\n");
+    puts("Enter `help` at any time to get extra information.");
+    puts("Type `exit` to close the shell.\n");
 }
